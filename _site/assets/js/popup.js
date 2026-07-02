f8ec640a4a3421feb7670376de9b818a74a6b448
Rituparna Sarkar <rituparnasarkr@gmail.com>
2026-07-02 12:08:12 +0530

    test

--- _site/assets/js/popup.js
+++ _site/assets/js/popup.js
@@ -2,15 +2,13 @@ const modal = document.getElementById("popup");
 const closeBtn = document.querySelector(".close");
 
 window.onload = () => {
-  modal.style.display = "block";
+  modal.classList.add("show");
 };
-
 closeBtn.onclick = () => {
-  modal.style.display = "none";
+  modal.classList.remove("show");
 };
-
 window.onclick = (event) => {
   if (event.target === modal) {
-    modal.style.display = "none";
+    modal.classList.remove("show");
   }
 };
