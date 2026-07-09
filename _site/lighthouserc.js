528255887909f5862e8e901931cbad05f1353025
Rituparna Sarkar <rituparnasarkr@gmail.com>
2026-07-09 16:09:49 +0530

    fixed lighthouse.cli yml

---
+++ _site/lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        'https://postdoc-potential.github.io/'
+      ]
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    }
+  }
+}
